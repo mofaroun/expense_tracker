86a0faab6874b9996766d0b28dd3e7e5d8877d5f
Mohammed Faroun <farounmohammed@gmail.com>
2024-08-17 16:51:57 -0500

    update header

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,21 @@
+import HeaderBox from '@/components/HeaderBox';
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = {firstName: "Guest"}
   return (
-    <div>Home</div>
-  )
-}
+    <section className='home'>
+      <div className='home-content'>
+        <header className = 'home-header'>
+          <HeaderBox
+            type = "greeting"
+            title = "welcome"
+            user = {loggedIn?.firstName || 'Guest'}
+            subtext = "Access and manage your account and transactions" />
+        </header>
+      </div>
+    </section>
+  );
+};
 
 export default Home
